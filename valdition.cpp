1ec2187269f3160bc489f0b3a33b1d1f7efcf2ac
mahmoud diaa eldeen kalaf <123967553+mahmoud-diaa-eldeen@users.noreply.github.com>
2024-05-13 23:08:25 +0300

    Update valdition.cpp

--- valdition.cpp
+++ valdition.cpp
@@ -1,89 +1,79 @@
+//#include"service.cpp"
+#include "repo.cpp"
 //**********************medicine*************************
 class valditionMedicine{
-	serviceMedicineImpl medicineService;
 	public :
-		void medicineValdition (medicine Medicine)
+		int medicineValdition (medicine Medicine)
 		{
-		if(Medicine.name[0]<65||Medicine.name[0]>90)
+		if(Medicine.getName()[0]<65||Medicine.getName()[0]>90)
 		{
 			cout<<"The medicine's name must start with capital letter, please enter it again:\n";
 			return -1;
 		}
-		else if (Medicine.takeNumber<1)
+		else if (Medicine.getTakeNumber()<1)
 		{
 			cout<<"The medicine's number of take medicine must be more than one, please enter it again:\n";
 				return -1;
 		}
 		else
-		 medicineService.addMedicine(Medicine);	
+		 return 1;	
 		}
 };
 //*************************doctor********************
 
 class valditionDoctor{
-	serviceDoctorImpl doctorService;
 	public:
-	void doctorValdition (doctor Doctor)
+	int doctorValdition (doctor Doctor)
 	{
-		if(Doctor.name[0]<65||Doctor.name[0]>90)
+		if(Doctor.getName()[0]<65||Doctor.getName()[0]>90)
 		{
 			cout<<"The doctor's name must start with capital letter, please enter it again:\n";
 			return -1;
 		}
-		else if(Doctor.phoneNumber.lenght!=11)
+		else if(Doctor.getPhoneNumber().length()!=11)
 		{
 			cout<<"The doctor's phone number must be 11 number, please enter it again:\n";
 			return -1;
 		}
-		else if(Doctor.age<25)
-		{
-			cout<<"The doctor's age must be more than 25, plase enter it again:\n";
-			return -1;
-		}
 		else
-		 doctorService.addDoctor(Doctor);	
+		 return 1;
 		}
+	};
 //********************************patient*********************
 class valditionPatient{
-	servicePatientImpl patientService;
 	public:
-	void patientValdition (patient Patient)
+	int patientValdition (patient Patient)
 	{
-		if(Patient.name[0]<65||Patient.name[0]>90)
+		if(Patient.getName()[0]<65||Patient.getName()[0]>90)
 		{
 			cout<<"The patient's name must start with capital letter, please enter it again:\n";
 			return -1;
 		}
-		else if(Patient.phoneNumber.lenght!=11)
+		else if(Patient.getPhoneNumber().length()!=11)
 		{
 			cout<<"The patient's phone number must be 11 number, please enter it again:\n";
 			return -1;
 		}
 		else
-		 patientService.addPatient(Patient);	
+		 return 1;	
 		}	
+		};
 //****************************staffinformation****************
-class valditionStaffinformation{
-	serviceStaffinformationImpl staffinformationService;
+class valditionStaffInf{
 	public:
-	void staffinformationValdition (staffinformation Staffinformation)
+	int staffInfValdition (staffInf Staffinformation)
 	{
-		if(Staffinformation.name[0]<65||Staffinformation.name[0]>90)
+		if(Staffinformation.getName()[0]<65||Staffinformation.getName()[0]>90)
 		{
 			cout<<"The staff member name must start with capital letter, please enter it again:\n";
 			return -1;
 		}
-		else if(Staffinformation.phoneNumber.lenght!=11)
+		else if(Staffinformation.getPhoneNumber().length()!=11)
 		{
 			cout<<"The staff member phone number must be 11 number, please enter it again:\n";
 			return -1;
 		}
-		else if(Staffinformation.age<25)
-		{
-			cout<<"The staff member age must be more than 25, plase enter it again:\n";
-			return -1;
-		}
 		else
-		 staffinformationService.addStaffinformation(Staffinformation);	
+		 return 1;
 		}
 };
